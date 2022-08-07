760608043fea6370da937f9214913c33bb566647
Charlotte Fereday <cbrfereday@gmail.com>
2022-08-07 16:49:23 +0100

    feat: configure nuxt config to find files in src folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    conponents: {
+        dirs: [
+            {
+                path: '~/components/',
+                extensions: ['vue'],
+            }
+        ]
+    },
+    srcDir: './src'
 
 })
