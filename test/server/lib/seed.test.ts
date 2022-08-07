b4a1a3274f26bbfdb4b4c29a931f52292ae1ea39
Charlotte Fereday <cbrfereday@gmail.com>
2022-08-07 16:52:19 +0100

    feat: reogranise files to src folder

--- test/server/lib/seed.test.ts
+++ test/server/lib/seed.test.ts
@@ -1,5 +1,5 @@
 import { it, expect } from "vitest";
-import { exampleSeed } from "../../../server/lib/seed"
+import { exampleSeed } from "../../../src/server/lib/seed"
 
 it("word should be  letters long", () => {
     expect(exampleSeed).toEqual("Some really awesome insight")
