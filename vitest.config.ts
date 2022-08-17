3ff54920d4a7183ae107631ee4ba64461188ba76
Charlotte Fereday <cbrfereday@gmail.com>
2022-08-17 18:34:26 +0100

    feat: adds vite-plugin-eslint

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
+import eslint from 'vite-plugin-eslint'
 
 // info here on how to configure vitest: https://vitest.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslint({
+    exclude: ['**/node_modules/**', '**/.vscode/**', '**/.nuxt/**', 'package.json', 'package-lock.json'],
+    failOnError: true
+  })],
   test: {
     deps: {
       inline: [/@nuxt\/test-utils-edge/],
