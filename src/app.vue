6a057efe060e6b9c7ef30062efbf1f1983cec597
sareg0 <regan.sara@gmail.com>
2022-10-12 20:37:38 +0200

    chore: npm update results + format files

--- src/app.vue
+++ src/app.vue
@@ -2,6 +2,15 @@
   <div>
     <h1>LoC Digital Garden</h1>
     <p>An aggregated list of technical learning from the community</p>
+    <br />
+    <Seed
+      :planted="3"
+      :tended="1"
+      :title="`Some title`"
+      :content="`Some learning content`"
+    />
+
+    <pre>{{ seed }}</pre>
     <NuxtPage />
   </div>
 </template>
