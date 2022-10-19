ce46e2009f85e188085e551a8aa71aea82296f7c
Charlotte Fereday <cbrfereday@gmail.com>
2022-10-19 20:03:50 +0100

    feat: updates e2e test

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,13 +1,14 @@
 import { describe, it, expect } from "vitest";
 import { setup, $fetch } from "@nuxt/test-utils-edge";
+import { load } from "cheerio";
 
 describe("My test", async () => {
   await setup({});
 
   it("displays expected content", async () => {
     const html = await $fetch("/");
-    expect(html).toContain(
-      "<h1>LoC Digital Garden</h1><p>An aggregated list of technical learning from the community</p>"
-    );
+    const elements = load(html);
+    const titles = elements("h1").text();
+    expect(titles).toBe("LoC Digital GardenTemplate for our Garden");
   });
 });
